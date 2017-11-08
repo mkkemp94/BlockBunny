ecf1d859c86977126d758a2bde93baaaef3ce832
Michael Kemp <mkemp@accu-time.com>
2017-11-08 15:28:15 -0500

    Introduced PPM - scale things to the correct size -- ie 100 pixels = 1 meter

---
+++ core/src/com/mygdx/blockbunny/handlers/B2DVars.java
@@ -0,0 +1,10 @@
+package com.mygdx.blockbunny.handlers;
+
+/**
+ * Created by mkemp on 11/8/17.
+ */
+
+public class B2DVars {
+
+    public static final float PPM = 100;
+}
--- core/src/com/mygdx/blockbunny/states/Play.java
+++ core/src/com/mygdx/blockbunny/states/Play.java
@@ -2,6 +2,7 @@ package com.mygdx.blockbunny.states;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
@@ -9,7 +10,10 @@ import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+import com.mygdx.blockbunny.Game;
 import com.mygdx.blockbunny.handlers.GameStateManager;
+
+import static com.mygdx.blockbunny.handlers.B2DVars.PPM;
 
 /**
  * Created by mkemp on 11/8/17.
@@ -20,6 +24,8 @@ public class Play extends GameState {
     private World world;
     private Box2DDebugRenderer b2dr;
 
+    private OrthographicCamera b2dCam;
+
     public Play(GameStateManager gsm) {
 
         super(gsm);
@@ -29,25 +35,29 @@ public class Play extends GameState {
 
         // Create platform
         BodyDef bodyDef = new BodyDef();
-        bodyDef.position.set(160, 120);
+        bodyDef.position.set(160 / PPM, 120 / PPM);
         bodyDef.type = BodyDef.BodyType.StaticBody;
         Body body = world.createBody(bodyDef);
 
         PolygonShape shape = new PolygonShape();
-        shape.setAsBox(50f, 5f);
+        shape.setAsBox(50 / PPM, 5 / PPM);
         FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = shape;
         body.createFixture(fixtureDef);
 
         // Create falling box
-        bodyDef.position.set(160, 200);
+        bodyDef.position.set(160 / PPM, 200 / PPM);
         bodyDef.type = BodyDef.BodyType.DynamicBody;
         body = world.createBody(bodyDef);
 
-        shape.setAsBox(5, 5);
+        shape.setAsBox(5 / PPM, 5 / PPM);
         fixtureDef.shape = shape;
         //fixtureDef.restitution = 1f;
         body.createFixture(fixtureDef);
+
+        // Set up box2d cam
+        b2dCam = new OrthographicCamera();
+        b2dCam.setToOrtho(false, Game.V_WIDTH / PPM, Game.V_HEIGHT / PPM);
     }
 
     @Override
@@ -66,7 +76,7 @@ public class Play extends GameState {
 
     @Override
     public void render() {
-        b2dr.render(world, cam.combined);
+        b2dr.render(world, b2dCam.combined);
     }
 
     @Override
