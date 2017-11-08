1120ee809653df4eb0497cbc7af858307a79d234
Michael Kemp <mkemp@accu-time.com>
2017-11-08 13:41:28 -0500

    Initial commit

---
+++ android/src/com/mygdx/blockbunny/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.blockbunny;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.blockbunny.BlockBunny;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BlockBunny(), config);
+	}
+}
